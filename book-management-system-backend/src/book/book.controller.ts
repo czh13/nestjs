4083d56edfe3b2f3fb34c00cf8bc010ab910912f
caizhihao <caizhihao@suntang.com>
2025-02-11 15:11:45 +0800

    图书管理系统接口开发

---
+++ book-management-system-backend/src/book/book.controller.ts
@@ -0,0 +1,69 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import type { UpdateBookDto } from './dto/update-book.dto';
+import type { CreateBookDto } from './dto/create-book.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from 'src/storge';
+import path from 'path';
+
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Get('list ')
+  async list() {
+    return this.bookService.list();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: number) {
+    return this.bookService.findById(id);
+  }
+
+  @Post('create')
+  async create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto);
+  }
+
+  @Post('update')
+  async update(@Body() updateBookDto: UpdateBookDto) {
+    return this.bookService.update(updateBookDto);
+  }
+
+  @Post('delete/:id')
+  async delete(@Param('id') id: number) {
+    return this.bookService.delete(id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, callback) {
+        const extname = path.extname(file.originalname);
+        if (['.png', '.jpg', '.gif'].includes(extname)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException('只能上传图片'), false);
+        }
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return file.path;
+  }
+}
--- book-management-system-backend/src/user/user.service.ts
+++ book-management-system-backend/src/user/user.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { DbService } from 'src/db/db.service';
+import { User } from './entities/user.entity';
+import type { LoginUserDto } from './dto/login-user.dto';
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  @Inject(DbService)
+  dbService: DbService;
+
+  async register(registerUserDto: RegisterUserDto) {
+    const users: User[] = await this.dbService.read();
+    const foundUser = users.find(
+      (item) => item.username === registerUserDto.username,
+    );
+    if (foundUser) {
+      throw new BadRequestException('该用户已经注册');
+    }
+    const user = new User();
+    user.username = registerUserDto.username;
+    user.password = registerUserDto.password;
+    users.push(user);
+    await this.dbService.write(users);
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async login(loginUserDto: LoginUserDto) {
+    const users: User[] = await this.dbService.read();
+    const foundUser = users.find(
+      (item) => item.username === loginUserDto.username,
+    );
+    if (!foundUser) {
+      throw new BadRequestException('用户不存在');
+    }
+    if (foundUser.password !== loginUserDto.password) {
+      throw new BadRequestException('密码错误');
+    }
+    return foundUser;
   }
 }
